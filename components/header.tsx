01c7dbd1c3204eefd07d865cc03286ad128692cf
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-10-13 17:55:27 -0500

    feat: add card and dropdown-menu components

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import { MenuIcon, StoreIcon, HomeIcon } from "lucide-react";
-import { Button } from "./ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
+import { StoreIcon, HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -31,7 +24,7 @@ export function Header() {
     <header className="w-full flex items-center justify-center h-[4rem] fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-sm border-b border-primary/10">
       <div className="flex items-center justify-between md:w-10/12 w-full max-w-6xl px-4 md:px-0">
         <p className="text-primary font-bold text-2xl">Esencia Antioqueña</p>
-        <div className="hidden md:flex gap-4">
+        <div className="flex gap-4">
           {menuItems
             .filter((item) => item.href !== pathname)
             .map((item) => (
@@ -43,27 +36,6 @@ export function Header() {
                 {item.icon} {item.label}
               </Link>
             ))}
-        </div>
-        <div className="md:hidden">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost">
-                <MenuIcon />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-screen left-0 right-0 rounded-none border-x-0 border-t-0">
-              {menuItems.map((item) => (
-                <DropdownMenuItem key={item.href} asChild>
-                  <Link
-                    href={item.href}
-                    className="flex items-center gap-2 w-full"
-                  >
-                    {item.icon} {item.label}
-                  </Link>
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
         </div>
       </div>
     </header>
