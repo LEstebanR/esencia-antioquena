34727ecbca256c657b37bb8ce61874aca7d1fb7b
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-10-15 21:45:02 -0500

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Header />
         <main className="mt-[4rem] md:mt-0">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
